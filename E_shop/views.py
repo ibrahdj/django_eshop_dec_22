145402be297ca3fc3a898816697bd4fbf47d3b2c
MACKBOOK <mackbook@MacBook-Pro-de-MACKBOOK.local>
2022-12-10 13:30:04 +0000

    dec_10_2

--- E_shop/views.py
+++ E_shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+  return render(request, "E_shop/index.html", {})
+
+def shop(request):
+    return render(request, "E_shop/shop.html", {})
+
--- django_eshop_dec_22/urls.py
+++ django_eshop_dec_22/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from E_shop import views
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('shop', views.shop, name='shop'),
 
 ]
